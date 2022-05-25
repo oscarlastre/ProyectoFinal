9c38435cf2d9d006b4ba8bd77c82ff1390fc1778
Oscar Lastre <oscarlastre@yahoo.com.ar>
2022-05-25 00:23:33 -0300

    Se crean archivos TypeScript

---
+++ src/app/components/hardsoftskills/hardsoftskills.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HardsoftskillsComponent } from './hardsoftskills.component';
+
+describe('HardsoftskillsComponent', () => {
+  let component: HardsoftskillsComponent;
+  let fixture: ComponentFixture<HardsoftskillsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HardsoftskillsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HardsoftskillsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/hardsoftskills/hardsoftskills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hardsoftskills',
+  templateUrl: './hardsoftskills.component.html',
+  styleUrls: ['./hardsoftskills.component.css']
+})
+export class HardsoftskillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
